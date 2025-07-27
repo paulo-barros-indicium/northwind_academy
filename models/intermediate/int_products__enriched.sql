48aa69fc0b088914f9e1f36d5b53ce70adf06872
paulo.barros@indicium.tech <paulo.barros@indicium.tech>
2025-07-27 02:18:33 +0000

    finalizada dimensao produtos

---
+++ models/intermediate/int_products__enriched.sql
@@ -0,0 +1,37 @@
+with
+    -- import models
+    products as (
+        select *
+        from {{ ref("stg_erp__products")}}
+    )
+    , suppliers as (
+        select *
+        from {{ ref("stg_erp__suppliers")}}
+    )
+    , categories as (
+        select *
+        from {{ ref("stg_erp__categories")}}
+    )
+    -- transformation
+    ,joined as (
+        select
+            products.product_pk
+            , products.product_name
+            , products.quantity_per_unity
+            , products.unit_price
+            , products.units_in_stock
+            , products.units_on_oder
+            , products.reorder_level
+            , products.is_discontinued as is_discontinued
+            , suppliers.supplier_name
+            , suppliers.supplier_city
+            , suppliers.supplier_country
+            , categories.category_name
+        from products
+        left join suppliers on products.supplier_fk = suppliers.supplier_pk
+        left join categories on products.category_fk = categories.category_pk        
+    )
+
+
+select *
+from joined
